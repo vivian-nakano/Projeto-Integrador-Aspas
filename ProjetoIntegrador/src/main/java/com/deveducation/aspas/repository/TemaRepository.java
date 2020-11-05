adbcc6a1f53ef19ad12b31295be25b47a723fcca
unknown <vrnakano@gmail.com>
2020-11-05 14:51:46 -0300

    projeto integrador

---
+++ ProjetoIntegrador/src/main/java/com/deveducation/aspas/repository/TemaRepository.java
@@ -0,0 +1,9 @@
+package com.deveducation.aspas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.deveducation.aspas.model.TemaModel;
+
+public interface TemaRepository extends JpaRepository<TemaModel, Long> {
+
+}
